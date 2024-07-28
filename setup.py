3affff2986d6e746afb269f5da3636d17ec95648
Saksham <sakshamshinde412@gmail.com>
2024-07-28 14:25:57 +0530

    Project setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Saksham Shinde',
+    author_email= 'sakshamshinde412@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
